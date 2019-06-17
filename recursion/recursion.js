f20dbaeba821a6666f950ba83e123c80b4c6fc79
dustyfingers <dustyfingers01257@gmail.com>
2019-06-17 08:58:32 -0600

    first and second recursive functions!

--- recursion/recursion.js
+++ recursion/recursion.js
@@ -1,1 +1,25 @@
 // a process or function is reursive if it calls *itself*
+// recursive functions have:
+// 1. a base case to terminate calls
+// 2. a different input each time the function is called
+
+
+//first recursive function
+function countDown(num) {
+    if (num <= 0) {
+        console.log('All done!');
+        return;
+    }
+    console.log(num);
+    num--;
+    countDown(num);
+}
+countDown(10);
+
+
+// second recursive function 
+function sumRange(num) {
+    if(num === 1) return 1;
+    return num + sumRange(num - 1);
+}
+console.log(sumRange(10));
