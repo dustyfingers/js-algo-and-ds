ce140dd7d6b184f40a1f07cb67d06dab0825093b
dustyfingers <dustyfingers01257@gmail.com>
2019-07-21 14:20:59 -0600

    reviewed instance methods

--- data-structures/classes.js
+++ data-structures/classes.js
@@ -4,9 +4,37 @@ class Student {
         this.firstName = firstName;
         this.lastName = lastName;
         this.year = year;
+        this.tardies = 0;
+        this.scores = [];
+    }
+    fullName() {
+        return `This student's full name is ${this.firstName} ${this.lastName}`;
+    }
+    makeLate() {
+        this.tardies += 1;
+        if (this.tardies >= 3) return `${this.firstName} ${this.lastName} has been expelled!`;
+        if (this.tardies === 1) return `${this.firstName} ${this.lastName} has been late ${this.tardies} time.`;
+        return `${this.firstName} ${this.lastName} has been late ${this.tardies} times.`;
+    }
+    addScore(score) {
+        this.scores.push(score);
+        return this.scores;
+    }
+    calculateAverage() {
+        if (this.scores.length === 0) return 'There are no scores to average';
+        let sum = this.scores.reduce((a, b) => a + b);
+        return Math.round(sum / this.scores.length);
     }
 }
 
 let firstStudent = new Student('Louie', 'Williford', 2016);
 
+console.log(firstStudent);
+console.log(firstStudent.fullName());
+console.log(firstStudent.makeLate());
+console.log(firstStudent.makeLate());
+console.log(firstStudent.addScore(100));
+console.log(firstStudent.addScore(87));
+console.log(firstStudent.addScore(94));
+console.log(firstStudent.calculateAverage());
 console.log(firstStudent);
