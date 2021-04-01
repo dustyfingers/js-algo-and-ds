02e71fc485f6fccd8005db138c159f4fb1c91a7a
lou <dustyfingers01257@gmail.com>
2021-04-01 12:21:22 -0600

    wrote pop and shift for a singly linked list

--- singly-linked-list/singly-linked-list.js
+++ singly-linked-list/singly-linked-list.js
@@ -27,10 +27,62 @@ class SinglyLinkedList {
             this.tail = newNode;
         }
 
-        // increment length
+        // increment length & return this
         this.length++;
-
         return this;
+    }
+
+    // traverse moves through each element of the list and logs the value held there
+    traverse() {
+        // place current at beginning
+        let current = this.head;
+        
+        // iterate through current & log value
+        while(current) {
+            console.log(current.val);
+            current = current.next;
+        }
+    }
+
+    // pop removes the last node from the linked list and returns it
+    pop() {
+        // short circuit
+        if (!this.head) return undefined;
+
+        let current = this.head;
+        let deletedItem;
+
+        while (current) {
+            // if list is only one item long
+            if (this.length == 1) {
+                deletedItem = current;
+                this.tail = null;
+                this.head = null;
+                this.length--;
+                return deletedItem;
+            }
+
+            // if at the second to last item
+            if (!current.next.next && this.length > 1) {
+                deletedItem = current.next;
+                current.next = null;
+                this.tail = current;
+                this.length--;
+                return deletedItem;
+            } else 
+            current = current.next;
+        }
+    }
+
+    // shift removes a node from the beginning from the linked list
+    shift() {
+        // short circuit
+        if (!this.head) return undefined;
+        let current = this.head;
+
+        this.head = current.next;
+        this.length--;
+        return current;
     }
 }
 
@@ -38,3 +90,18 @@ let list = new SinglyLinkedList();
 
 console.log(list.push("Hi!"));
 console.log(list.push("150"));
+
+list.traverse();
+
+console.log(list.pop());
+console.log(list);
+console.log(list.pop());
+console.log(list);
+
+
+console.log(list.push("random!"));
+console.log(list.push("junk"));
+console.log(list.push("here!"));
+
+console.log(list.shift());
+console.log(list);
