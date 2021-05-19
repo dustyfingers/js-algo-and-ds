cdea64533f46fcaf5bb52052035d94b9a75843dd
lou <dustyfingers01257@gmail.com>
2021-05-19 13:24:27 -0600

    implemented depth first search (pre order variant) on a binary search tree

--- trees/BinarySearchTree.js
+++ trees/BinarySearchTree.js
@@ -70,20 +70,36 @@ class BinarySearchTree {
 
         return visited;
     }
+
+    depthFirstSearchPreOrder() {
+        let visited = [],
+            currentNode = this.root;
+        
+        const dfsHelper = node => {
+            visited.push(node.value);
+            if (node.left) dfsHelper(node.left);
+            if (node.right) dfsHelper(node.right);
+        }
+        
+        dfsHelper(currentNode);
+        return visited;
+    }
+
 }
 
 let tree = new BinarySearchTree();
 
 tree.insert(10)
-tree.insert(5);
+tree.insert(6);
 tree.insert(15);
-tree.insert(9);
-tree.insert(13);
-tree.insert(25);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
 // console.log(tree.find(10));
 // console.log(tree.find(2));
 
 console.log(tree.breadthFirstSearch());
+console.log(tree.depthFirstSearchPreOrder());
 
 // big o of binary search trees
 // O(log n) for insertion (NOT guaranteed - this is AT BEST)
