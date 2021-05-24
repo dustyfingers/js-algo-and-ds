612ad9274ecc5b9bc1e4aec9e9746abfc118454f
lou <dustyfingers01257@gmail.com>
2021-05-24 13:53:24 -0600

    implemented a simple hash table class with a set function

--- hash-tables/hash-table.js
+++ hash-tables/hash-table.js
@@ -1,16 +1,23 @@
-function hash(key, arrLength) {
-    let result = 0,
-        p = 15485863;
-
-    for (let i = 0; i < Math.min(key.length, 100); i++) {
-        let char = key[i];
-        let value = char.charCodeAt(0) - 96;
-        result = (result * p + value) % arrLength;
+class HashTable {
+    constructor(size=53) {
+        this.keyMap = new Array(size);
     }
-    return result;
+
+    _hash (key) {
+        let result = 0,
+            p = 15485863;
+    
+        for (let i = 0; i < Math.min(key.length, 100); i++) {
+            let char = key[i];
+            let value = char.charCodeAt(0) - 96;
+            result = (result * p + value) % this.keyMap.length;
+        }
+        return result;
+    }
+
+    set (key, value) {
+        let index = this._hash(key);
+        if (!this.keyMap[index]) this.keyMap[index] = [];
+        this.keyMap[index].push([key, value]);
+    }
 }
-
-let stuff = "hello bello woo hoo";
-
-console.log(hash(stuff, 20));
-console.log(hash(stuff + 'r', 20));
