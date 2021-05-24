71c30cfcf8518763e18bb40f36c288a67494347c
lou <dustyfingers01257@gmail.com>
2021-05-24 14:27:13 -0600

    implemented keys and values functions on a hash table class

--- hash-tables/hash-table.js
+++ hash-tables/hash-table.js
@@ -30,6 +30,34 @@ class HashTable {
         }
         return undefined;
     }
+
+    values() {
+        let valuesArr = [];
+        for (let i = 0; i < this.keyMap.length; i++) {
+            if (this.keyMap[i]) {
+                for (let j = 0; j < this.keyMap[i].length; j++) {
+                    if (!valuesArr.includes(this.keyMap[i][j][1])) {
+                        valuesArr.push(this.keyMap[i][j][1]);
+                    }
+                }
+            }
+        }
+        return valuesArr;
+    }
+
+    keys() {
+        let keysArr = [];
+        for (let i = 0; i < this.keyMap.length; i++) {
+            if (this.keyMap[i]) {
+                for (let j = 0; j < this.keyMap[i].length; j++) {
+                    if (!keysArr.includes(this.keyMap[i][j][0])) {
+                        keysArr.push(this.keyMap[i][j][0]);
+                    }
+                }
+            }
+        }
+        return keysArr;
+    }
 }
 
 let ht = new HashTable();
@@ -39,3 +67,5 @@ ht.set('green', '#grnhex');
 ht.set('red', '#redhex');
 
 console.log(ht.get('yellow'));
+console.log(ht.values());
+console.log(ht.keys());
