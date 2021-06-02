e631ae7bcc2302fb8704d81fb79966b38369f205
lou <dustyfingers01257@gmail.com>
2021-06-01 18:00:32 -0600

    learned about graph traversal and implemented a recursive DFS algorithm on a graph

--- graphs/graph.js
+++ graphs/graph.js
@@ -25,13 +25,19 @@ class Graph {
         delete this.adjacencyList[v];
     }
 
-    recursiveDFS(v) {
-        let res = [],
+    recursiveDFS(start) {
+        const res = [],
             visited = {};
 
-        const dfsHelper = (vt) => {
-            if (!this.adjacencyList[vt]) return undefined;
+        const dfsHelper = vt => {
+            if (!vt) return undefined;
+            visited[vt] = true;
+            res.push(vt);
+            this.adjacencyList[vt].forEach(neighbor => {
+                if (!visited[neighbor]) return dfsHelper(neighbor);
+            });
         }
+        dfsHelper(start);
 
         return res;
     }
@@ -40,12 +46,31 @@ class Graph {
 let g = new Graph();
 g.addVertex('tokyo');
 g.addVertex('dallas');
+g.addVertex('san francisco');
+g.addVertex('bengaluru');
 g.addVertex('morocco');
-g.addVertex('semey');
-g.addEdge('tokyo', 'semey')
-g.addEdge('tokyo', 'morocco')
-g.addEdge('tokyo', 'dallas')
-g.addEdge('dallas', 'morocco')
-g.removeEdge('tokyo', 'dallas')
-g.removeVertex('tokyo');
-console.log(g);
+g.addVertex('hong kong');
+g.addVertex('miami');
+g.addVertex('new york');
+g.addVertex('sydney');
+g.addVertex('dubai');
+g.addVertex('lisbon');
+g.addVertex('paris');
+g.addEdge('san francisco', 'bengaluru');
+g.addEdge('san francisco', 'miami');
+g.addEdge('san francisco', 'sydney');
+g.addEdge('san francisco', 'dubai');
+g.addEdge('san francisco', 'lisbon');
+g.addEdge('paris', 'dubai');
+g.addEdge('paris', 'san francisco');
+g.addEdge('bengaluru', 'dubai');
+g.addEdge('bengaluru', 'morocco');
+g.addEdge('bengaluru', 'dallas');
+g.addEdge('dallas', 'new york');
+g.addEdge('tokyo', 'new york');
+g.addEdge('tokyo', 'morocco');
+g.addEdge('tokyo', 'dallas');
+g.addEdge('miami', 'sydney');
+g.addEdge('lisbon', 'dubai');
+g.addEdge('dallas', 'morocco');
+console.log(g.recursiveDFS('san francisco'));
