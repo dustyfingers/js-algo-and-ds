be0efac58cab40ef8d713681e207f81f001602d4
lou <dustyfingers01257@gmail.com>
2021-06-02 20:36:25 -0600

    learned about priority queues and implemented a priority queue class

---
+++ binary-heaps/priority-queue.js
@@ -0,0 +1,81 @@
+class Node {
+    constructor(val, priority) {
+        this.val = val;
+        this.priority = priority;
+    }
+}
+
+class PriorityQueue {
+    constructor() {
+        this.values = [];
+    }
+
+    enqueue(val, priority) {
+        const node = new Node(val, priority);
+        this.values.push(node);
+        this.bubbleUp();
+    }
+
+    bubbleUp() {
+        let idx = this.values.length - 1;
+        const element = this.values[idx];
+
+        while(idx > 0) {
+            let parentIdx = Math.floor((idx - 1) / 2);
+            let parent = this.values[parentIdx];
+            if (element.priority <= parent.priority) break;
+            [this.values[parentIdx], this.values[idx]] = [this.values[idx], this.values[parentIdx]];
+            idx = parentIdx;
+        }
+    }
+
+    dequeue() {
+        const max = this.values[0];
+        const end = this.values.pop();
+        if (this.values.length > 0) {
+            this.values[0] = end;
+            this.sinkDown();
+        }
+        return max;
+    }
+
+    sinkDown() {
+        let idx = 0;
+        const { length } = this.values, element = this.values[0];
+
+        while (true) {
+            const leftChildIdx = 2 * idx + 1, rightChildIdx = 2 * idx + 2;
+            let leftChild, rightChild, swap = null;
+
+            if (leftChildIdx < length) {
+                leftChild = this.values[leftChildIdx];
+                if (leftChild.priority > element.priority) swap = leftChildIdx;
+            }
+            if (rightChildIdx < length) {
+                rightChild = this.values[rightChildIdx];
+                if (
+                    (!swap && rightChild.priority > element.priority) || 
+                    (swap && rightChild.priority > leftChild.priority)) swap = rightChildIdx;
+            }
+            if (swap === null) break;
+            [this.values[idx], this.values[swap]] = [this.values[swap], this.values[idx]];
+            idx = swap;
+        }
+        return element;
+    }
+}
+
+let queue = new PriorityQueue();
+queue.enqueue('common cold', 2);
+queue.enqueue('gunshot wound', 10);
+queue.enqueue('seizure', 3);
+queue.enqueue('broken bone', 6);
+queue.enqueue('headache', 1);
+console.log(queue.values);
+queue.dequeue();
+console.log(queue.values);
+queue.dequeue();
+queue.dequeue();
+queue.dequeue();
+queue.dequeue();
+console.log(queue.values);
