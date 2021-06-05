ebb03a83e935531c65f62db4108c451c4ca4ca04
lou <dustyfingers01257@gmail.com>
2021-06-05 12:47:06 -0600

    memoized the fibonacci function

--- dynamic-programming/dynamic-fibonacci.js
+++ dynamic-programming/dynamic-fibonacci.js
@@ -1,12 +1,20 @@
-const fib = n => {
+const fib = (n, memo=[]) => {
+    if (memo[n] !== undefined) return memo[n];
     if (n <= 2) return 1;
-    return fib(n-1) + fib(n-2);
+    const res = fib(n-1, memo) + fib(n-2, memo);
+    memo[n] = res;
+    return res;
 }
 
-console.log(fib(45));
+console.log(fib(100));
 
 // what about the time complexity fo this solution?
 // its.... not good. 
 // O(2^n)
 // even worse that quadratic (n^2), like REAL bad
 // almost as bad as O(n!) which is just insane tbh
+
+// enter memoization!
+// we can store the result of expensive function calls and 
+// return the cached result when the same inputs occour again!
+// for example so we dont have to figure out fib(5) EVERY single time we need it
