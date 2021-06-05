08d6a1d1acc998a017fc747eac63e55effeadcf9
lou <dustyfingers01257@gmail.com>
2021-06-05 12:51:08 -0600

    analyzed the time complexity of the memoized solution

--- dynamic-programming/dynamic-fibonacci.js
+++ dynamic-programming/dynamic-fibonacci.js
@@ -1,3 +1,4 @@
+// the memo is like the lookup table for past solutions!
 const fib = (n, memo=[]) => {
     if (memo[n] !== undefined) return memo[n];
     if (n <= 2) return 1;
@@ -18,3 +19,8 @@ console.log(fib(100));
 // we can store the result of expensive function calls and 
 // return the cached result when the same inputs occour again!
 // for example so we dont have to figure out fib(5) EVERY single time we need it
+
+// what about the big O of the memoized solution?
+// its time complexity is actually O(n)!
+// way better!
+// at scale, this technique could be very useful in certain contexts...
